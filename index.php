aac53d4fc0c9f97a998dbce287a13df868f3eb4e
Valik <valikfad@gmail.com>
2021-09-21 16:13:48 +0300

    update index.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     To: <input type="datetime-local" name="to"><br>
     Description: <input type="text" name="description"><br>
     Address: <input type="text" name="address"><br>
+    <input type="radio" id="google" name="system" value="google" checked>
+    <label for="google">Google</label><br>
+    <input type="radio" id="yahoo" name="system" value="yahoo">
+    <label for="yahoo">Yahoo</label><br>
     <input type="submit">
 </form>
 <?php
@@ -12,7 +16,7 @@ require_once("vendor/autoload.php");
 
 use Spatie\CalendarLinks\Link;
 
-if (isset($_POST['title']) && isset($_POST['from']) && isset($_POST['to']))
+if (isset($_POST['title']) && isset($_POST['from']) && isset($_POST['to']) && isset($_POST['system']))
 {
     $dateFrom = str_replace("T", " ", $_POST['from']);
     $from = DateTime::createFromFormat('Y-m-d H:i', $dateFrom);
@@ -23,6 +27,7 @@ if (isset($_POST['title']) && isset($_POST['from']) && isset($_POST['to']))
     $link = Link::create($_POST['title'], $from, $to)
         ->description($_POST['description'])
         ->address($_POST['address']);
-    echo "<a href='" . $link->google() . "' target='_blank'>Google Calendar Link </a>";
+
+    echo "<a href='" . $link->{$_POST['system']}() . "' target='_blank'>Link</a>";
 }
 ?>
